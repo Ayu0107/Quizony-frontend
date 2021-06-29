aeb2a7d0873858b26151c3a8d106d9ab7e33dc06
Ayu0107 <aggarwalayushi0109@gmail.com>
2021-06-29 20:40:43 +0530

    navbar responsive

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function myFunction() {
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {
-	  x.className += " responsive";
+		x.className += " responsive";
+		document.getElementById("dropdown-hover-link").style.display = "block";
+		document.getElementById("dropdown-hover-link2").style.display = "block";
 	} else {
-	  x.className = "topnav";
+		x.className = "topnav";
+		document.getElementById("dropdown-hover-link").style.display = "none";
+		document.getElementById("dropdown-hover-link2").style.display = "none";
 	}
-  }
+}
